d96173d1fdfb8e0700d207f36e162b952ae76106
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2021-11-05 15:58:40 +0000

    Create order
    Creates an order using requests.
    Order contains a stop loss and take profit.

--- alpaca_api.py
+++ alpaca_api.py
@@ -10,12 +10,31 @@ import requests
 #API URLS
 BASE_URL = "https://paper-api.alpaca.markets"
 ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
+ORDERS_URL = "{}/v2/orders".format(BASE_URL)
 
 #ALPACA MODULE
 api = tradeapi.REST(API_KEY, SECRET_KEY, BASE_URL)
 
 #Creates an order with a stop loss and take profit
+def createOrder(symbol, qty, side, take_profit, stop_loss):
+    data ={
+        "symbol": symbol,
+        "qty": qty,
+        "side": side,
+        "type": "market",
+        "order_class": "bracket",
+        "time_in_force": "day",
+        "take_profit": {
+            "limit_price": take_profit
+        },
+        "stop_loss": {
+            "stop_price": stop_loss
+        }
+    }
+    r = requests.post(ORDERS_URL, json=data, headers=HEADERS)
+    data = r.json()
 
+    return data
 
 #Closes any pending orders or open positions
 

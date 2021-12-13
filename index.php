c3554bfc0d6980cd89b7e4d8d4a327c46e7f4a97
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2021-12-13 17:19:31 +0000

    Initial PHP page
    Tested with Wamps server for Local host

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<HTML>
+    <HEAD>
+        <title>
+            Hello World!
+        </title>
+    </HEAD>
+
+    <BODY>
+        <h1>Heading 1</h1>
+        <p>This is the body of the site</p>
+    </BODY>
+</HTML>
+

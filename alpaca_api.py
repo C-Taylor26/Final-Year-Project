7ff6414f73d4b4ab2922115705f35de6f7243fd9
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2021-12-18 13:12:59 +0000

    getsCandle Fuction
    Gets a given dates candle chart

--- alpaca_api.py
+++ alpaca_api.py
@@ -7,6 +7,10 @@ import alpaca_trade_api as tradeapi
 #imprt requests for api calls
 import requests
 
+#Datatime module for managing time objects
+import datetime
+
+
 #API URLS
 BASE_URL = "https://paper-api.alpaca.markets"
 ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
@@ -52,7 +56,19 @@ def getEquity():
     return equity
 
 #Gets bar sets (Candle stick cart)
+def getCandle(symbol, date):
+    day = int(date.split('-')[0])
+    month = int(date.split('-')[1])
+    year = int(date.split('-')[2])
 
+    now = datetime.datetime.now()
+    start = now.replace(year=year, month=month, day=day, hour = 0, minute=0, second=0, microsecond=0).isoformat() + "Z"
+    end = now.replace(year=year, month=month, day=day, hour = 23, minute=59, second=59, microsecond=0).isoformat() + "Z"
+
+    r = api.get_barset(symbol, "5Min", start=start, end=end)
+    data = r._raw
+
+    data = data["TSLA"][1:]
+    return data
 
 #Gets open positions for a given stock
-

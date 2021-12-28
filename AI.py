b8ae81b94da466c394d2f7068577ada1e2a5e0d6
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2021-12-28 01:32:57 +0000

    Updated to work with new File name

--- AI.py
+++ AI.py
@@ -1,5 +1,3 @@
-from ctypes import DEFAULT_MODE
-from hashlib import new
 from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPRegressor
@@ -21,11 +19,12 @@ def demoFunc():
 
     pass
 
-def newFunc():
+def predictAll():
+
     fields = ["Five_Minute_Change", "Average_Volume", "Days_Change", "20MA_Change", "50MA_Change", "200MA_Change"]
-    inputs = pd.read_csv("csvData - Copy.csv", usecols=fields)
+    inputs = pd.read_csv("dataAll19-21.csv", usecols=fields)
     fields = ["Next_Days_Change"]
-    targets = pd.read_csv("csvData - Copy.csv", usecols=fields)
+    targets = pd.read_csv("dataAll19-21.csv", usecols=fields)
 
     inputs = inputs.values
     targets = targets.values
@@ -37,5 +36,20 @@ def newFunc():
     regressor.fit(scaled, targets)
     outputs = regressor.predict(scaled)
     print(np.mean(abs(outputs-targets)))
+
+    i = 0
+    correct = 0
+    for item in outputs:
+        if item < 0 and targets[i] < 0:
+            correct +=1
+        elif item > 0 and targets[i] > 0:
+            correct +=1
+        i +=1
     
+    percent = (correct / i) *100
+
+    print ("Change Direction Correct - {}".format(correct))
+    print ("Correct {}% of the time".format(percent))
+
+
 newFunc()

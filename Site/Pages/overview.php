3bb2247034c65a5c425b461ee87104fe62bf76bc
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-02-04 17:39:08 +0000

    Page Plan
    Basic planned roughly divided and commented

---
+++ Site/Pages/overview.php
@@ -0,0 +1,46 @@
+<HTML>
+<head>
+    <title>Dashboard</title>
+    <!-- BANNER -->
+</head>
+<body>
+    <div id="left">
+        <div id="t-l">
+            <h1 id="dateTime"></h1>
+        </div>
+        <div id="m-l">
+            <!--Chart-->
+        </div>
+    </div>
+    <div id="middle">
+        <div id="t-m">
+            <!--Change %/$-->
+        </div>
+        <div id="m-m">
+            <!--chart options-->
+        </div>
+        <div id="b-m">
+
+        </div>
+    </div>
+    <div id="right">
+        <div id="t-r">
+            <!--secondary Chart-->
+        </div>
+        <div id="m-r">
+            <!--secondary Chart options-->
+        </div>
+        <div id="b-r">
+            <!--button to other page-->
+        </div>
+    </div>
+
+</body>
+<script>
+
+</script>
+
+<footer>
+    <!--FOOTER-->
+</footer>
+</HTML>

e3c513c0c0ea9e597c8f327c17fb846bf99eb011
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-02-05 14:43:36 +0000

    Clock Added
    JS clock added to overview page

--- Site/Pages/overview.php
+++ Site/Pages/overview.php
@@ -3,6 +3,7 @@
     <title>Dashboard</title>
     <!-- BANNER -->
 </head>
+
 <body>
     <div id="left">
         <div id="t-l">
@@ -35,10 +36,36 @@
         </div>
     </div>
 
-</body>
-<script>
+    <script>
+        function timeUpdate() { //Updates Clock every 10000ms to correct time
+            let now = new Date()
 
-</script>
+            let hours = now.getHours().toString()
+            if (hours.length === 1) { hours = "0" + hours }
+
+            let minutes = now.getMinutes().toString()
+            if (minutes.length === 1) { minutes = "0" + minutes }
+
+            let year = now.getFullYear().toString()
+
+            let month = (now.getMonth()+1).toString()
+            if (month.length === 1) { month = "0" + month }
+
+            let day = (now.getDay()-1).toString()
+            if (day.length === 1) { day = "0" + day }
+
+            let time = hours + ":" + minutes
+            let date = day + "/" + month + "/" + year
+
+            document.getElementById("dateTime").innerHTML = time + " | " + date
+            setTimeout(timeUpdate, 10000)
+        }
+
+        timeUpdate()
+    </script>
+
+</body>
+
 
 <footer>
     <!--FOOTER-->

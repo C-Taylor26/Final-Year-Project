eaf019227adbc35770ec85917fa38f4e0a19e145
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-02-06 20:49:37 +0000

    Nav Bar

--- Site/Pages/overview.php
+++ Site/Pages/overview.php
@@ -1,49 +1,55 @@
 <HTML>
 
-
 <!-- Bootstrap -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+-->
 <!-- JS Charts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 
 <head>
+    <link rel="stlyesheet" href="../CSS/banner.css"
+
     <title>Dashboard</title>
     <!-- BANNER -->
 </head>
 
 <body>
+    <header>
+        <nav>
+            <ul class="nav-bar">
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Monthly Data</a></li>
+            </ul>
+        </nav>
+
+
+    </header>
+
+
+
     <div id="content">
         <div id="left" style="float: left;width: 70%; padding: 1%;">
             <div id="t-l">
                 <h1 id="dateTime"></h1>
+                <h2 id="change" style="float: right">Change: 50% | $100</h2>
             </div>
             <div id="m-l">
                 <!--Chart-->
                 <canvas id="mainChart" style="width:100%;"></canvas>
             </div>
         </div>
-        <div id="middle" style="width: 0%; float: left">
-            <div id="t-m">
-                <!--Change %/$-->
-            </div>
-            <div id="m-m">
-                <!--chart options-->
-            </div>
-            <div id="b-m">
-
-            </div>
-        </div>
         <div id="right" style="float: right;width: 30%; padding: 1rem; padding-top: 5rem;">
             <div id="t-r">
                 <!--secondary Chart-->
+                <h3 style="float: right">Stock Performance</h3>
+                <canvas id="secondaryChart" style="width: 100%; float: left"
             </div>
             <div id="m-r">
                 <!--secondary Chart options-->
             </div>
             <div id="b-r">
                 <!--button to other page-->
-                <button type="button" class="btn btn-outline-warning" style="width: 90%">Monthly Data</button>
+                <button type="button" class="btn btn-outline-warning" style="width: 100%">Monthly Data</button>
             </div>
         </div>
     </div>
@@ -62,7 +68,7 @@
             let month = (now.getMonth()+1).toString()
             if (month.length === 1) { month = "0" + month }
 
-            let day = (now.getDay()-1).toString()
+            let day = (now.getDate()).toString()
             if (day.length === 1) { day = "0" + day }
 
             let time = hours + ":" + minutes
@@ -97,6 +103,32 @@
             });
         }
 
+        function lineChartSide() { //Displays line chart of results
+            let xValues = [50,60,70,80,90,100,110,120,130,140,150];
+            let yValues = [7,8,8,9,9,9,10,11,14,14,15];
+
+            new Chart("secondaryChart", {
+                type: "line",
+                data: {
+                    labels: xValues,
+                    datasets: [{
+                        fill: false,
+                        lineTension: 0,
+                        backgroundColor: "rgba(0,0,255,1.0)",
+                        borderColor: "rgba(0,0,255,0.1)",
+                        data: yValues
+                    }]
+                },
+                options: {
+                    legend: {display: false},
+                    scales: {
+                        yAxes: [{ticks: {min: 6, max:16}}],
+                    }
+                }
+            });
+        }
+
+        lineChartSide()
         lineChartMain()
         timeUpdate()
 

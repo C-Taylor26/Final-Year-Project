f4bb5d67181574f844c4f7a54c08e50f3bec9d38
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-03-04 10:32:19 +0000

    Login Page

--- Site/index.php
+++ Site/index.php
@@ -2,57 +2,97 @@
 ?>
 <HTML>
     <HEAD>
-        <link rel="stylesheet" href="style.css">
-        <title>Dashboard</title>
+        <title>Login</title>
+
+        <style>
+            .loginBox {
+                overflow: hidden;
+                border: 1px solid #ccc;
+                background-color: #f1f1f1;
+            }
+            .loginBox button {
+                background-color: inherit;
+                float: left;
+                border: none;
+                outline: none;
+                cursor: pointer;
+                padding: 14px 16px;
+                transition: 0.3s;
+            }
+
+            .loginBox button:hover {
+                background-color: #ddd;
+            }
+
+            .loginBox button.active {
+                background-color: #ccc;
+            }
+
+            .tabcontent {
+                display: none;
+                padding: 6px 12px;
+                border: 1px solid #ccc;
+                border-top: none;
+            }
+
+
+        </style>
+
+
 
     </HEAD>
 
     <BODY>
     <div id="main">
-        <h1>Trader Dashboard</h1>
-        <div class="row">
-            <div class="column left">
-                <h2>Account Value:</h2>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
-                <canvas id="myChart" style="width:50%;"></canvas>
-                <script>
-                    var xValues = [50,60,70,80,90,100,110,120,130,140,150];
-                    var yValues = [7,8,8,9,9,9,10,11,14,14,15];
+        <div class="loginBox">
+            <button class="tab" onclick="showContent(event, 'login')" id="default">Login</button>
+            <button class="tab" onclick="showContent(event, 'register')">Register</button>
+        </div>
+        <!-- Login Box Tab Contents -->
+        <div id="login" class="tabcontent">
+            <h1>LOGIN</h1>
 
-                    new Chart("myChart", {
-                        type: "line",
-                        data: {
-                            labels: xValues,
-                            datasets: [{
-                                fill: false,
-                                lineTension: 0,
-                                backgroundColor: "rgba(0,0,255,1.0)",
-                                borderColor: "rgba(0,0,255,0.1)",
-                                data: yValues
-                            }]
-                        },
-                        options: {
-                            legend: {display: false},
-                            scales: {
-                                yAxes: [{ticks: {min: 1, max:16}}],
-                            }
-                        }
-                    });
-                </script>
-            </div>
-            <div class="column right">
-                <div id="changeNumms">
-                    <p class="stats">[50% | +$1000]</p>
-                </div>
-                <div id="pieChart">
 
-                </div>
-                <div id="dataButton">
 
-                </div>
-            </div>
+        </div>
+
+        <div id="register" class="tabcontent">
+            <h1>REGISTER</h1>
+
+
+
+
         </div>
     </div>
+
+    <script>
+        function showContent(event, option) {
+            // Declare all variables
+            var i, tabcontent, tab;
+
+            // Get all elements with class="tabcontent" and hide them
+            tabcontent = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabcontent.length; i++) {
+                tabcontent[i].style.display = "none";
+            }
+
+            // Get all elements with class="tablinks" and remove the class "active"
+            tabs = document.getElementsByClassName("tab");
+            for (i = 0; i < tabs.length; i++) {
+                tabs[i].className = tabs[i].className.replace(" active", "");
+            }
+
+            // Show the current tab, and add an "active" class to the button that opened the tab
+            document.getElementById(option).style.display = "block";
+            event.currentTarget.className += " active";
+        }
+
+        document.getElementById("default").click();
+
+    </script>
+
+
+
     </BODY>
 </HTML>
 

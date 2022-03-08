de978998de12233f07cf61277cc038398201a67c
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-03-08 00:20:40 +0000

    Added Login Funcitonality

--- Site/PHP/login.php
+++ Site/PHP/login.php
@@ -1,6 +1,8 @@
 <?php
 include_once "dbConnection.php";
-
+if (!isset($_SESSION)){
+    session_start();
+}
 
 try {
     $email = $_POST["email"];
@@ -9,14 +11,30 @@ try {
     $saltedPW = SALT . $pw;
 
     $user = getUser($email);
+    $login = false;
+    if (count($user)>0) {
+        $pwActual = $user[0]["password"];
 
-    if (count($user)>0){
-        $userPass = $user[0]
+        for ($i = 0; $i < strlen(PEPPER_CHARS); $i++) {
+            $testPW = $saltedPW . PEPPER_CHARS[$i];
+            $testHash = strtoupper(hash("sha256", $testPW));
+            if ($testHash === $pwActual) {
+                echo "LOGIN";
+                $login = true;
+            }
+        }
+    }
+    if ($login === true){
+        $_SESSION["user"] = $email;
+        $_SESSION["value"] = $user[0]["value"];
+        $_SESSION["fname"] = $user[0]["fName"];
+        $_SESSION["lname"] = $user[0]["lName"];
+        header("Location: ../Pages/overview.php");
+    }
+    else{
+        header("Location: ../Pages/index.php?error=login-unsuccessful");
     }
 
-    for ($i=0; $i < 26; $i++){
-
-    }
 }
 catch (Exception $e){
     echo $e;

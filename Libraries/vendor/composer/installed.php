0fcd8e220ce841f9fe572d7abb04e1e22c662a7f
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-03-14 17:42:29 +0000

    MFA Library

---
+++ Libraries/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd9b679d609cb5ab4ca5c9c9403f8a80be2c759c2',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd9b679d609cb5ab4ca5c9c9403f8a80be2c759c2',
+            'dev_requirement' => false,
+        ),
+        'robthree/twofactorauth' => array(
+            'pretty_version' => '1.8.1',
+            'version' => '1.8.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../robthree/twofactorauth',
+            'aliases' => array(),
+            'reference' => '5afcb45282f1c75562a48d479ecd1732c9bdb11b',
+            'dev_requirement' => false,
+        ),
+    ),
+);

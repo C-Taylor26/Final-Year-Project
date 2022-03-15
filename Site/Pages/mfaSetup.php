d59fe5f4ed1ec3a13cf9eac4565e91a7b27c4a4f
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-03-15 00:02:06 +0000

    MFA Setup
    Used to setup MFA for new users

--- Site/Pages/mfaSetup.php
+++ Site/Pages/mfaSetup.php
@@ -1,18 +1,28 @@
 <?php
+require_once "../../Libraries/vendor/autoload.php";
 
 if (!isset($_SESSION)){
     session_start();
 }
-var_dump($_SESSION);
 
 //Setting up MFA Library
-require_once "../../Libraries/vendor/autoload.php";
 use RobThree\Auth\TwoFactorAuth;
 $tfa = new TwoFactorAuth();
 
-//Generating Secret
-$secret = $tfa->createSecret();
+if (!isset($_SESSION["email"])){
+    header("Location: index.php");
+}
+if (!isset($_SESSION["token"])){
+    $_SESSION["token"] = $tfa->createSecret();
+}
 
+
+//Showing Error message if required
+if(isset($_GET["error"])){
+    echo "<script>window.addEventListener('load', (event) => {showError();});</script>";
+}
+
+var_dump($_SESSION);
 ?>
 
 <HTML>
@@ -24,35 +34,27 @@ $secret = $tfa->createSecret();
     <style>
         .c-r {
             float: right;
-            width: 48%;
+            width: 50%;
             padding: 10px;
             text-align: center;
         }
         .c-l {
             float: left;
-            width: 48%;
-            margin: 10px;
+            width: 50%;
             padding: 10px;
             text-align: center;
         }
 
-        h1 {
-            align-content: center;
-        }
         .main{
-
+            width: 100%;
+            padding: 2.5%;
+            text-align: center;
         }
-        /*Disabling number input arrows*/
-        /* Chrome, Safari, Edge, Opera */
-        input::-webkit-outer-spin-button,
-        input::-webkit-inner-spin-button {
-            -webkit-appearance: none;
-            margin: 0;
-        }
-
-        /* Firefox */
-        input[type=number] {
-            -moz-appearance: textfield;
+        .lower{
+            width: 20%;
+            margin: auto;
+            align-content: center;
+            text-align: center;
         }
     </style>
 
@@ -60,32 +62,44 @@ $secret = $tfa->createSecret();
 
 <body>
 
-<div class="main">
+<div class="row">
     <div id="MFA-Description" class="c-l">
         <h1>MFA</h1>
         <p>MFA or Multi-Factor Authentication is used to ensure an increased level of security for a users account.</p>
         <p>In order to use this service please setup MFA using one of the following applications:</p>
-        <p><a href="https://support.google.com/accounts/answer/1066447?hl=en&co=GENIE.Platform%3DAndroid">Google Authenticator</a></p>
-        <p><a href="https://www.microsoft.com/en-us/security/mobile-authenticator-app">Microsoft Authenticator</a></p>
+        <p><a href="https://support.google.com/accounts/answer/1066447?hl=en&co=GENIE.Platform%3DAndroid" target="_blank">Google Authenticator</a></p>
+        <p><a href="https://www.microsoft.com/en-us/security/mobile-authenticator-app" target="_blank">Microsoft Authenticator</a></p>
 
     </div>
 
     <div id="MFA-Code" class="c-r">
         <h1>MFA Code</h1>
         <p>Scan the QR below within your choosen application, or use the 16-digit code</p>
-        <img src="<?php echo $tfa->getQRCodeImageAsDataUri("AI Trading", $secret);?>" alt="QR Code">
-        <p><?php echo chunk_split($secret,4, " ");?></p>
+        <img src="<?php echo $tfa->getQRCodeImageAsDataUri("AI Trading", $_SESSION["token"]);?>" alt="QR Code">
+        <p><?php echo chunk_split($_SESSION["token"],4, " ");?></p>
+    </div>
+
+</div>
+<div class="row">
+    <div id="verify-Code" class="lower">
+        <h1>Verify Code</h1>
+        <p>Use the code displayed within your application to verify your identity</p>
+        <form action="../PHP/verifyCode.php?r=setup" method="post">
+            <input type="text" min="6" maxlength="6" required name="code">
+            <div class="alert alert-danger" id="codeError" style="display: none;">
+                Incorrect Code
+            </div>
+            <button type="submit" class="btn btn-success">Verify</button>
+        </form>
     </div>
 </div>
 
-<div id="verify-Code" class="lower">
-    <h1>Verify Code</h1>
-    <p>Use the code displayed within your application to verify your identity</p>
-    <form action="../PHP/verifyCode?r=setup.php" method="post">
-        <input type="text" min="6" maxlength="6" required>
-        <button type="submit" class="btn btn-success">Verify</button>
-    </form>
-</div>
+<script>
+    function showError() {
+        let errormsg = document.getElementById("codeError");
+        errormsg.style.display = "block";
+    }
+</script>
 
 </body>
 

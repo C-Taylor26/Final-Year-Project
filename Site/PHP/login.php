bd62332da065f3a0545c84170515cd582b037c2c
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-03-15 00:02:52 +0000

    Update login.php
    Sets session variables when successful

--- Site/PHP/login.php
+++ Site/PHP/login.php
@@ -1,45 +1,39 @@
 <?php
 include_once "dbConnection.php";
+include_once "settings.php";
+
 if (!isset($_SESSION)){
     session_start();
-    session_destroy();
 }
 
-try {
-    $email = $_POST["email"];
-    $pw = $_POST["pw"];
+$email = $_POST["email"];
+$pw = $_POST["pw"];
 
-    $saltedPW = SALT . $pw;
+$saltedPW = SALT . $pw;
 
-    $user = getUser($email);
-    $login = false;
-    if (count($user)>0) {
-        $pwActual = $user[0]["password"];
+$user = getUser($email);
+$login = false;
+if (count($user)>0) {
+    $pwActual = $user[0]["password"];
 
-        for ($i = 0; $i < strlen(PEPPER_CHARS); $i++) {
-            $testPW = $saltedPW . PEPPER_CHARS[$i];
-            $testHash = strtoupper(hash("sha256", $testPW));
-            if ($testHash === $pwActual) {
-                echo "LOGIN";
-                $login = true;
-            }
+    for ($i = 0; $i < strlen(PEPPER_CHARS); $i++) {
+        $testPW = $saltedPW . PEPPER_CHARS[$i];
+        $testHash = strtoupper(hash("sha256", $testPW));
+        if ($testHash === $pwActual) {
+            $login = true;
         }
     }
-    if ($login === true){
-        $_SESSION["user"] = $user;
-        if ($user[0]["mfaKey"] === ''){
-            header("Location: ../Pages/mfaSetup.php");
-        }
-        else{
-            header("Location: ../Pages/overview.php");
-        }
-
-    }
-    else{
-        header("Location: ../Pages/index.php?error=login-unsuccessful");
-    }
-
 }
-catch (Exception $e){
-    echo $e;
+if ($login === true){
+    $_SESSION["email"] = $user[0]["ID"];
+    $_SESSION["fname"] = $user[0]["fName"];
+    $_SESSION["lname"] = $user[0]["lName"];
+    $_SESSION["value"] = $user[0]["value"];
+    $_SESSION["token"] = $user[0]["mfaToken"];
+    $_SESSION["auth"] = false;
+    header("Location: ../Pages/mfaCheck.php");
 }
+else{
+    header("Location: ../Pages/index.php?error=login-unsuccessful");
+}
+

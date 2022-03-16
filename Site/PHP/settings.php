4bc0f664f8edec9f0ca683fe6f7608a09affcda9
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-03-16 22:39:34 +0000

    Added AES variables

--- Site/PHP/settings.php
+++ Site/PHP/settings.php
@@ -6,3 +6,6 @@ const DB_PASSWORD = '';
 const DB_DATABASE = 'fyp';
 const SALT = "i%l]C;H";
 const PEPPER_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const CIPHER = 'aes-256-cbc';
+const KEY = 'sgjRpMn3C3bmupd5ZIe76mftTO28KOsK';
+const IV = 5786420745348263;

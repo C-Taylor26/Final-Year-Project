e21d4c889db91c7184722a5df09f4807b410a4e2
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-04-08 00:59:47 +0100

    Neural Network Code

---
+++ Program/NeuralNetwork.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+class NeuralNetwork:
+    def __init__(self, layers, alpha=0.1):
+        self.W = []
+        self.layers = layers
+        self.alpha = alpha
+
+        for i in np.arange(0, len(layers) - 2):
+            w = np.random.randn(layers[i] + 1, layers[i + 1] + 1)
+            self.W.append(w / np.sqrt(layers[i]))
+        w = np.random.randn(layers[-2] + 1, layers[-1])
+        self.W.append(w / np.sqrt(layers[-2]))
+
+    def sigmoid(self, x):
+        return 1.0 / (1 + np.exp(-x))
+    
+    def sigmoid_deriv(self, x):
+        return x * (1 - x)
+
+    def fit(self, X, y, epochs=1000, displayUpdate=100):
+        percentageStep = epochs/10
+        X = np.c_[X, np.ones((X.shape[0]))]
+
+        for epoch in np.arange(0, epochs):
+            for (x, target) in zip(X, y):
+                self.fit_partial(x, target)
+
+            if (epoch + 1) % percentageStep == 0:
+                percentage = ((epoch+1)/percentageStep)*10
+                loss = self.calcLoss(X, y)
+                print("PROGRESS - {}%\t Current Loss: {}".format(int(percentage), loss))
+            elif epoch == 0:
+                loss = self.calcLoss(X, y)
+                print("STARTING LOSS: {}".format(loss))        
+  	
+    def fit_partial(self, x, y):
+        A = [np.atleast_2d(x)]
+
+        for layer in np.arange(0, len(self.W)):
+            net = A[layer].dot(self.W[layer])
+            out = self.sigmoid(net)
+            A.append(out)
+
+        error = A[-1] - y
+
+        D = [error * self.sigmoid_deriv(A[-1])]
+
+        for layer in np.arange(len(A) - 2, 0, -1):
+            delta = D[-1].dot(self.W[layer].T)
+            delta = delta * self.sigmoid_deriv(A[layer])
+            D.append(delta)
+
+        D = D[::-1]
+        for layer in np.arange(0, len(self.W)):
+            self.W[layer] += -self.alpha * A[layer].T.dot(D[layer])
+
+    def predict(self, X, addBias=True):
+
+        p = np.atleast_2d(X)
+        if addBias:
+            p = np.c_[p, np.ones((p.shape[0]))]
+        for layer in np.arange(0, len(self.W)):
+            p = self.sigmoid(np.dot(p, self.W[layer]))
+        return p            
+    
+    def calcLoss(self, X, targets):
+        targets = np.atleast_2d(targets)
+        predictions = self.predict(X, addBias=False)
+        loss = 0.5 * np.sum((predictions - targets) ** 2)
+        # return the loss
+        return loss   
+    
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+nn = NeuralNetwork([2, 2, 2, 1], 0.5)
+nn.fit(X, y, 10000)

6f84c0b0352c103eb922490eb00c3e5515e82792
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-04-08 22:09:51 +0100

    Update dbConnection.py

--- Program/dbConnection.py
+++ Program/dbConnection.py
@@ -1,9 +1,15 @@
 #MySQL Module for database connections
 import mysql.connector
+import hashlib
 
 def getStockData(stock, dataType):
     sql = "SELECT * FROM data WHERE datatype='{}' AND stock='{}' ORDER BY date".format(dataType, stock)
-    return dbSatement(sql)
+    results = dbSatement(sql)
+    """if validateHash(results):
+        return results
+    else:
+        return "Validatoin Issue"""
+    return results
 
 def dbSatement(sql):
     db = mysql.connector.connect(host="localhost", user="root", password="",database="fyp")
@@ -12,7 +18,19 @@ def dbSatement(sql):
     results = cursor.fetchall()
     return results
 
-results = getStockData("NFLX", "testing")
+def validateHash(data):
+    #date as sting
+    #input = "{}, '{}'".format(values, hashlib.sha256(values.encode()).hexdigest())
+    #MAs = "{}, {}, {}, {}, {}, {}".format(oMAs.twenty, oMAs.fifty, oMAs.twoHundered, cMAs.twenty, cMAs.fifty, cMAs.twoHundered)
+    #date = data.date.strftime("%Y-%m-%d")
+    #values = "'{}', '{}', {}, {}, {}, {}, {}, '{}'".format(data.stock, date, data.daysVolume, data.daysOpen, data.daysClose, MAs, data.nextDayChange, datatype)
+    for x in data:
+        hash = x[14]
+        datapoints = "'{}', '{}', {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, '{}'".format(x[1],x[2],x[3],x[4],x[5],x[6],x[7],x[8],x[9],x[10],x[11],float(x[12]), x[13])
+        testHash = hashlib.sha256(datapoints.encode()).hexdigest()
+        if hash == testHash:
+            return False
 
-for x in results:
-    print (x)
+    return True
+
+results = getStockData("NFLX", "testing")

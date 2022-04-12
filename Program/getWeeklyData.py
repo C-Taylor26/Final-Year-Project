b2a548f5c8e34f514468f1f25de1d90a900166c8
Callumt683 <92585232+C-Taylor26@users.noreply.github.com>
2022-04-12 15:32:32 +0100

    Weekly Data
    Used to test NN further as weekly data may give better trend indications

---
+++ Program/getWeeklyData.py
@@ -0,0 +1,72 @@
+import requests
+from keys import *
+
+class Week:
+    def __init__(self, date, open, close, change, maChanges):
+        self.date = date
+        self.open = open
+        self.close = close
+        self.change = change
+        self.maChanges = maChanges
+
+def getWeekly(stock):
+    json = {"function":"TIME_SERIES_WEEKLY","symbol":stock,"datatype":"json"}
+    r = requests.request("GET", VANTAGE_URL, headers=VANTAGE_HEADERS, params=json)
+    data = r.json()
+
+    data = data['Weekly Time Series']
+    weeklyData = []
+    count = -1
+    for week in data:
+        if count == -1:
+            count +=1
+            continue
+        elif count > 77:
+            break
+        else:
+            open = float(data[week]["1. open"])
+            close = float(data[week]["4. close"])
+            newWeek = Week(week, open, close, ((close-open)/open), {"20":0,"50":0,"200":0})
+            weeklyData.append(newWeek)
+            count +=1
+        
+    weeklyData = getMAs(stock, weeklyData)
+    
+    return weeklyData
+
+def getMAs(stock, weeklyData):
+    for tp in ["20", "50", "200"]:
+        json = {"time_period":tp,
+                       "interval":"daily",
+                       "series_type":"close",
+                       "function":"SMA",
+                       "symbol":stock,
+                       "datatype":"json"}
+        r = requests.request("GET", VANTAGE_URL, headers=VANTAGE_HEADERS, params=json)
+        data = r.json()
+        data = data["Technical Analysis: SMA"]
+
+        maArray = []
+        for item in data:
+            maArray.append({"date":item,"ma":data[item]["SMA"]})
+
+        i = 0
+        for week in weeklyData:
+            i = 0
+            while True:
+                if maArray[i]["date"] == week.date:
+                    close = float(maArray[i]["ma"])
+                    open = float(maArray[i+4]["ma"])
+                    change = (close - open)/open
+                    week.maChanges[tp] = change
+                    break
+                i +=1
+    
+    return weeklyData
+
+        
+
+data = getWeekly("TSLA")
+print(len(data))
+print(data[0].open)
+print(data[-1].close)
--- Program/keys.py
+++ Program/keys.py
@@ -1,3 +1,5 @@
 API_KEY = "PKYR0RU8RBFHON0A69GB"
 SECRET_KEY = "tnmSaP94cZfBjAZeh80MBThl5tZ7j1oD2zuZnA5N"
 HEADERS = {'APCA-API-KEY-ID': API_KEY, 'APCA-API-SECRET-KEY': SECRET_KEY}
+VANTAGE_HEADERS = {"X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com","X-RapidAPI-Key": "e46c232eb6msh8c8bdb02adafd02p1864fdjsn5ceadcdc1b95"}
+VANTAGE_URL = "https://alpha-vantage.p.rapidapi.com/query"
